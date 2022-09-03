adb75da4a7ba40b2655ae0cd2e274aa1f6b6ed34
Dinalisse Felicione <dinalisse@google.com>
2022-09-03 19:04:55 +0000

    remove slash at end if url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // backend_route: 'http://localhost:3000/api',
   backend_route: 'https://serene-inlet-74805.herokuapp.com/api',
-  frontend_route: 'https://gswe-dol-tracker2.herokuapp.com/'
+  frontend_route: 'https://gswe-dol-tracker2.herokuapp.com'
 };
