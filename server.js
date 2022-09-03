f5df3f9f1a0bef158cb295a344b2a4d083bf05b2
Dinalisse Felicione <dinalisse@google.com>
2022-09-03 21:03:28 +0000

    changes to server

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist'))
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'))})
+    res.sendFile(path.join(__dirname + '/dist'))})
 
 app.listen(process.env.PORT || 4200)
