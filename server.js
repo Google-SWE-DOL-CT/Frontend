8ea10445f5616cb65ffbfa9b564512792b1663e8
Dinalisse Felicione <dinalisse@google.com>
2022-09-03 22:13:50 +0000

    modify server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 
-app.use(express.static('./dist'))
+app.use(express.static('./dist/dol-swe-comp'))
 
 app.get('/*', (req,res)=>{
     res.sendFile('index.html', {root:'dist/dol-swe-comp'})})
