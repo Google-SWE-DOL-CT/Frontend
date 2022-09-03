bdc381b823278b3f1e4bc7a3773cf66d80d62fc7
Dinalisse Felicione <dinalisse@google.com>
2022-09-03 21:55:24 +0000

    changes

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express')
-const path = require('path')
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static('./dist'))
 
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist'))})
+app.get('/*', (req,res)=>{
+    res.sendFile('index.html', {root:'dist/dol-swe-comp'})})
 
-app.listen(process.env.PORT || 4200)
+app.listen(process.env.PORT || 8080)
