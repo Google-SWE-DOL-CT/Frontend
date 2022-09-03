c6f62b72b099bdf7afab21ef62e18c064a0be96f
Dinalisse Felicione <dinalisse@google.com>
2022-09-03 20:52:43 +0000

    reconfigure server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path')
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/dol-swe-comp'))
+app.use(express.static(__dirname + '/dist'))
 
-app.get('/*', (req,res)=>{
-    res.sendFile('index.html', {root:'dist/dol-swe-comp'})})
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname + '/dist/index.html'))})
 
-app.listen(process.env.PORT || 4300)
+app.listen(process.env.PORT || 4200)
