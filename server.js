d84da951805263ac65423d6af3be0d4bd4058487
Dinalisse Felicione <dinalisse@google.com>
2022-09-03 19:17:05 +0000

    add index.html to get

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/dol-swe-comp'))
 
-app.get('/*', function(req,res){res.sendFile(path.join(__dirname + '/dist/dol-swe-comp'))})
+app.get('/*', function(req,res){res.sendFile(path.join(__dirname + '/dist/dol-swe-comp/index.html'))})
 
 app.listen(process.env.PORT || 4300)
