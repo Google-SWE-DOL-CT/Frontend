9c81ab62eebedb7c1db27bc210efe546b2713a49
CJGarciaMusic <CJGarciaMusic@gmail.com>
2022-09-07 12:10:40 -0400

    removed unused impaments

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -9,14 +9,13 @@ import { AdminRoot } from '../admin-data';
 import { AdminService } from '../admin.service';
 import { User, UserRoot } from '../user';
 import { UserService } from '../user.service';
-import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css']
 })
-export class AdminDashboardComponent implements OnInit, AfterViewInit {
+export class AdminDashboardComponent implements OnInit {
 
   sub!: Subscription
   adminsub!: Subscription
